4711edd435c6110a9107054aee46b3326bc3ac4d
Lee Hanbyeol <leehanbyeol971219@gmail.com>
2023-11-28 04:20:33 +0900

    feat : 채팅 기능 구현 중

--- src/pages/chat/ChatMessageInput.tsx
+++ src/pages/chat/ChatMessageInput.tsx
@@ -7,33 +7,9 @@ const ChatMessageInput = () => {
   const { currentMessage, setCurrentMessage } = useMesssageStore();
   const { currentInputError, setCurrentInputError } = useInputErrorStore();
 
-  return (
-    <>
-      <S.MessageInput
-        placeholder="메세지를 작성하시오."
-        id="chatMessage"
-        value={currentMessage}
-        onChange={(e) => {
-          setCurrentMessage(e.target.value);
-          setCurrentInputError(false);
-        }}
-        hasError={currentInputError}
-      />
-    </>
-  );
+  return <></>;
 };
 
 export default ChatMessageInput;
 
-const S = {
-  MessageInput: styled.input<{ hasError: boolean }>`
-    padding: 13px;
-    min-height: 40px;
-    min-width: 80%;
-    outline: none;
-    border: ${({ hasError }) => (hasError ? '1px solid red' : 'none')};
-    font-size: ${styleFont.body3};
-    background-color: ${COLORS.GRAY[200]};
-    border-radius: 50px;
-  `
-};
+const S = {};
