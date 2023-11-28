01f5d21da4d4413c73f76c383758c510d4192e7d
KimYoonSu97 <zkzk625@naver.com>
2023-11-28 12:05:27 +0900

    feat : 유저 스토어 수정

--- src/api/AxiosInstance.ts
+++ src/api/AxiosInstance.ts
@@ -33,4 +33,34 @@ axiosInstance.interceptors.request.use(
   (error: AxiosError) => Promise.reject(error)
 );
 
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const {
+      config,
+      response: { status }
+    } = error;
+
+    if (error) {
+      const originalRequest = config;
+      const newAtk = await getAccessTokenWhenExpiration();
+      if (newAtk) {
+        console.log('newAtk', newAtk);
+        originalRequest.headers['Authorization'] = newAtk.atk;
+        localStorage.setItem('atk', newAtk.atk);
+        return axiosInstance(originalRequest);
+      } else {
+        localStorage.removeItem('atk');
+        localStorage.removeItem('rtk');
+        localStorage.removeItem('id');
+        alert('로그인이 만료되었습니다. 다시 로그인해주세요.');
+        // userStore.logout();
+        Navigate({ to: '/login' });
+      }
+    }
+  }
+);
+
 export default axiosInstance;
