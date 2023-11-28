c75800b37631742ab8e6af64c2a32b4fd04a2f7a
hwang daeseong <klp2797@naver.com>
2023-11-28 23:18:33 +0900

    feat: 포인트충전 완료, 세부 스타일 작업 진행중

--- src/components/auth/PointCharge.tsx
+++ src/components/auth/PointCharge.tsx
@@ -1,105 +1,34 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import axiosInstance from 'src/api/AxiosInstance'
 import userStore from 'src/store/userStore'
 import { COLORS } from 'src/styles/styleConstants'
 import { styleFont } from 'src/styles/styleFont'
+import { RequestPayParams, RequestPayResponse } from 'src/types/mypage/PointCharge'
 import styled from 'styled-components'
 
-export interface RequestPayAdditionalParams {
-  digital?: boolean;
-  vbank_due?: string;
-  m_redirect_url?: string;
-  app_scheme?: string;
-  biz_num?: string;
-}
 
-export interface Display {
-  card_quota?: number[];
-}
-
-export interface RequestPayParams extends RequestPayAdditionalParams {
-  pg?: string;
-  pay_method: string;
-  escrow?: boolean;
-  merchant_uid: string;
-  name?: string;
-  amount: number;
-  custom_data?: any;
-  tax_free?: number;
-  currency?: string;
-  language?: string;
-  buyer_name?: string;
-  buyer_tel: string;
-  buyer_email?: string;
-  buyer_addr?: string;
-  buyer_postcode?: string;
-  notice_url?: string | string[];
-  display?: Display;
-}
-
-export interface RequestPayAdditionalResponse {
-  apply_num?: string;
-  vbank_num?: string;
-  vbank_name?: string;
-  vbank_holder?: string | null;
-  vbank_date?: number;
-}
-
-export interface RequestPayResponse extends RequestPayAdditionalResponse {
-  success: boolean;
-  error_code: string;
-  error_msg: string;
-  imp_uid: string | null;
-  merchant_uid: string;
-  pay_method?: string;
-  paid_amount?: number;
-  status?: string;
-  name?: string;
-  pg_provider?: string;
-  pg_tid?: string;
-  buyer_name?: string;
-  buyer_email?: string;
-  buyer_tel?: string;
-  buyer_addr?: string;
-  buyer_postcode?: string;
-  custom_data?: any;
-  paid_at?: number;
-  receipt_url?: string;
-}
-
-export type RequestPayResponseCallback = (response: RequestPayResponse) => void;
-
-export interface Iamport {
-  init: (accountID: string) => void;
-  request_pay: (params: RequestPayParams, callback?: RequestPayResponseCallback) => void;
-}
-
-declare global {
-  interface Window {
-    IMP?: Iamport;
-  }
-}
-
-const payOption = ["1만","5만","10만","100만"]
+const payOption = ["+1만", "+5만", "+10만", "+100만"]
 
 const PointCharge = () => {
   const { profile: user } = userStore();
-  const [payValue,setPayValue] = useState<number>(0)
+  const [payValue, setPayValue] = useState(0)
+  const navigate = useNavigate()
 
   const pointChargeButton = () => {
-   
+
     /* 1. 가맹점 식별하기 */
-    if(!window.IMP) {
-     return;
+    if (!window.IMP) {
+      return;
     }
-     const { IMP } = window;
-     IMP.init('imp60571512');
-    
+    const { IMP } = window;
+    IMP.init('imp60571512');
+
 
 
     /* 2. 결제 데이터 정의하기 */
-    const data:RequestPayParams = {
+    const data: RequestPayParams = {
       pg: 'kakaopay',                           // PG사
       pay_method: 'card',                           // 결제수단
       merchant_uid: `mid_${new Date().getTime()}`,   // 주문번호
@@ -117,7 +46,7 @@ const PointCharge = () => {
   }
 
   /* 3. 콜백 함수 정의하기 */
-  const callback = async(response:RequestPayResponse) => {
+  const callback = async (response: RequestPayResponse) => {
     const {
       success,
       merchant_uid,
@@ -126,58 +55,82 @@ const PointCharge = () => {
     console.log(response)
 
     if (success) {
-      const res = await axiosInstance.put('/home/users/cash',{
-       cash : response!.paid_amount
+      const res = await axiosInstance.put('/home/users/cash', {
+        cash: response!.paid_amount
       })
       console.log(res)
       toast('결제가 완료되었습니다.')
+      navigate('/mypage')
     } else {
-      alert(`결제 실패: ${error_msg}`);
+      toast(`결제 실패: ${error_msg}`);
     }
-   // navigate('/mypage/point-charge')
- }
+  }
 
- const payValueHandler = (e:React.ChangeEvent<HTMLInputElement>) => {
-  const value = e.target.value
-    setPayValue(priceFormat(value))
- }
+  const payValueHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target as HTMLInputElement;
+    const removedCommaValue = value.replaceAll(",", "")
+    setPayValue(priceFormat(removedCommaValue.toLocaleString()))
+  }
 
- const priceFormat = (pay:any) => {
+  // const addComma = (pay:string) => {
+  //   const returnString = pay?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  //   return returnString
+  // }
 
-    const numberPay = parseInt(pay,10)
-    if(!isNaN(numberPay)){
-       return numberPay
+  const priceFormat = (pay: string) => {
+    const numberPay = parseInt(pay)
+    if (isNaN(numberPay)) {
+      return 0;
+    } else if (numberPay >= 10000) {
+      return Math.ceil(numberPay / 1000) * 1000
+    } else {
+      return numberPay
     }
-    return 0;
-    
- }
+
+
+  }
+
+  const easyPointButton = (easyPoint: string) => {
+    if (easyPoint === "+1만") {
+      setPayValue(payValue + 10000)
+    } else if (easyPoint === "+5만") {
+      setPayValue(payValue + 50000)
+    } else if (easyPoint === "+10만") {
+      setPayValue(payValue + 100000)
+    } else if (easyPoint === "+100만") {
+      setPayValue(payValue + 1000000)
+    }
+  }
+
 
   return (
     <S.Container>
       <S.Title>포인트 충전</S.Title>
       <S.PointBefore>
-      <S.PointRow>
-            <h2>결제 포인트</h2>
-            <S.InputWrap>
-            <input  value={payValue} onChange={payValueHandler}></input><span>원</span>
-            {payOption.map((pay)=>{
-              return <p>{pay}</p>
-            })}
-            </S.InputWrap>
-          </S.PointRow>
-          <S.PointRow>
-            <h2>보유 포인트</h2>
-            <p>{user?.cash.toLocaleString()}원</p>
-          </S.PointRow>
+        <S.PointRow>
+          <h2>결제 포인트</h2>
+          <S.InputWrap>
+            <p><input placeholder='충전금액 입력' value={payValue.toLocaleString()} onChange={payValueHandler} maxLength={9}></input>원</p>
+            <S.EasyPointWrap>
+              {payOption.map((pay) => {
+                return <button key={pay} onClick={() => easyPointButton(pay)}>{pay}</button>
+              })}
+            </S.EasyPointWrap>
+          </S.InputWrap>
+        </S.PointRow>
+        <S.PointRow>
+          <h2>보유 포인트</h2>
+          <p>{user?.cash.toLocaleString()}원</p>
+        </S.PointRow>
 
-        </S.PointBefore>
-        <S.PointAfter>
-          <S.PointRow>
-            <h2>결제 후 포인트</h2>
-            <p>{(payValue)}원</p>
-          </S.PointRow>
-          <button onClick={pointChargeButton}>결제하기</button>
-        </S.PointAfter>
+      </S.PointBefore>
+      <S.PointAfter>
+        <S.PointRow>
+          <h2>결제 후 포인트</h2>
+          <p>{(payValue + (user?.cash || 0)).toLocaleString()}원</p>
+        </S.PointRow>
+        <button onClick={pointChargeButton}>결제하기</button>
+      </S.PointAfter>
     </S.Container>
   )
 }
@@ -185,11 +138,11 @@ const PointCharge = () => {
 export default PointCharge
 
 const S = {
-  Container:styled.div`
+  Container: styled.div`
   width: 100%;
   max-width: 400px;
   `,
-  Title:styled.h1`
+  Title: styled.h1`
         display: flex;
     justify-content: center;
     padding: 5.5rem;
@@ -201,7 +154,7 @@ const S = {
     margin-bottom: 15px;
   }
 `,
-PointAfter: styled.div`
+  PointAfter: styled.div`
   div {
     margin: 15px 0px;
   }
@@ -224,33 +177,45 @@ PointAfter: styled.div`
     }
   }
 `,
-PointRow: styled.div`
+  PointRow: styled.div`
   display: flex;
-  align-items: center;
+  align-items: top;
   justify-content: space-between;
   h2 {
     ${styleFont.body1}
   }
 
   p {
+    text-align: right;
     ${styleFont.h1}
   }
   .pointColor {
     color: ${COLORS.GREEN[300]};
   }
 `,
-InputWrap:styled.p`
+  InputWrap: styled.div`
   display: flex;
   gap: 5px;
+  flex-direction: column;
   justify-content: end;
   align-items: center;
   input{
-    width: 80%;
+    width: 88%;
     border-radius: 5px;
     ${styleFont.h1};
     border-color :${COLORS.GRAY[400]};
     outline:none;
     text-align: right;
+  }
+`,
+  EasyPointWrap: styled.div`
+  display: flex;
+
+  gap: 5px;
+  button{
+    border: solid 1px ${COLORS.GRAY[400]};
+    border-radius: 4px;
+    padding: 2px 11px;
   }
 `
 }
